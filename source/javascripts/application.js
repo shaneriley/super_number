325b8fdc3cb17609519636a5d52101603294c3d4
Cameron Daigle <camerondaigle@gmail.com>
2013-10-18 15:08:53 -0400

    Phase 1 of fading numbers idea

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -37,55 +37,24 @@ $(function() {
     $("input.hours").trigger("superNumber.decrement");
   });
 
-  var clouds = {
-    $el : $("#clouds"),
-    tierCount: 4,
-    cloudCount: 5,
+  var numbers = {
+    $el: $("#numbers"),
+    number_of_doom: "94143243431512659321054872390486828512913474876027671959234602385829583047250165232525929692572765536436346272718401201264314754632945012784726484107562234789626728592858295347502772262646456217613984829519475412398501",
     getRange: function(min, max) {
-      return min + Math.random() * (max - min);
+      return Math.round(min + Math.random() * (max - min));
     },
-    drawTier: function(num) {
-      for (var i = 0; i < this.cloudCount; i++) {
-        this.drawCloud(num);
-      }
-    },
-    drawCloud: function(tier) {
-      var $c = $("<div />", { "class": "cloud" }),
-          last_y = 0,
-          row_length = this.getRange(5, 15);
-      for (var row = 0; row < this.getRange(2, 5); row++) {
-        var last_x = this.getRange(50, 100) * row,
-            y = row  * (tier * 0.7) * (100 * (1 - row * 0.2));
-        for (var bulge = 0; bulge < row_length; bulge++) {
-          var dims = this.getRange(150, 200) * (tier * 0.7) * (1 - row * 0.2);
-          var $span = $("<span />");
-          $span.css({
-            width: dims,
-            height: dims,
-            left: last_x,
-            bottom: y
-          });
-          last_x = last_x + this.getRange(0, dims * 0.5);
-          $c.append($span);
-        }
-        row_length = row_length * 0.7;
-      }
-      var duration = this.getRange(60 * (tier + 1) * 0.8, 60 * (tier + 1) * 1.2),
-          delay = this.getRange(-duration, 0);
-      $c.css({
-        opacity: 1 - (tier * 0.2),
-        bottom: - $c.children().first().height() / 2,
-        "-webkit-animation": "drift " + duration + "s " + delay + "s infinite"
-      });
-      this.$el.prepend($c);
-    },
-    draw: function() {
-      for (var i = 0; i <= this.tierCount; i++) {
-        this.drawTier(i);
+    init: function() {
+      for(var i=0; i<this.number_of_doom.length; i++) {
+        var $span = $("<span />");
+        $span.text(this.number_of_doom[this.getRange(0, this.number_of_doom.length-1)]);
+        $span.css({
+          "-webkit-animation": "pulse " + this.getRange(3, 60) + "s infinite linear"
+        });
+        this.$el.append($span);
       }
     }
   };
 
-  clouds.draw();
+  numbers.init();
 
 });
